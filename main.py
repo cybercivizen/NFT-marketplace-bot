aba4ed73550a87dbd72788f5925065788ecdcdff
0xcydg <khalil.eljouadi@hotmail.com>
2022-11-02 15:13:47 +0100

    Modified class name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
